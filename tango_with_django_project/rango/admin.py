8b288ab694d93f6bb374623c9caab7f9f25a27c1
Chad Stovern <chadhs@digitalnomad.im>
2015-03-09 09:36:22 -0500

    completed chapter 7 'Models, Templates and Views' and exercises.

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -4,5 +4,9 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'views', 'likes')
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
